8f833fd5fd931f2ba9b422ea6d15ad63a598c2a2
theBook273 <votansach273@gmail.com>
2023-03-09 23:13:39 +0700

    comment added

--- capSoCong.cpp
+++ capSoCong.cpp
@@ -10,12 +10,14 @@ int main()
 
     sort(a, a + 3);
 
+    // tìm giá trị chênh lệch
     long long spaceT = min(a[1] - a[0], a[2] - a[1]);
 
     for (long long i = 0; i < 3; i++)
     {
         if (a[i] + spaceT != a[i + 1])
         {
+            // tìm kết quả
             cout << a[i] + spaceT;
             return 0;
         }
